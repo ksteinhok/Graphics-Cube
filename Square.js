4958549d57674e801d4e6d441ca9126ae8006c30
ksteinhok <73804015+ksteinhok@users.noreply.github.com>
2023-03-08 00:01:48 -0800

    partial cube

---
+++ Square.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Square.js
+//
+
+function Square(gl, vertexShader, fragmentShader) {
+
+    vertexShader ||= "Square-vertex-shader";
+    fragmentShader ||= "Square-fragment-shader";
+
+    let program = initShaders(gl, vertexShader, fragmentShader);
+
+    // Set up our data:
+    //   - positions contains our vertex positions
+    //   - indices contains how to organize the vertices
+    //       into primitives
+    //
+    let positions = [
+        0.0, 0.0, 0.0,  // Vertex 0
+        1.0, 0.0, 0.0,// Vertex 1
+        0.0, 1.0, 0.0, // Vertex 2
+        1.0, 1.0, 0.0,  // Vertex 3
+        0.0, 0.0, 1.0, // Vertex 4
+        1.0, 0.0, 1.0,  // Vertex 5
+        0.0, 1.0, 1.0,  // Vertex 6
+        1.0, 1.0, 1.0 // vertex7
+    ];
+
+    let indices = [
+        1,2,0,
+        1,3,2,
+        //5,1,3,
+        //3,7,5,
+        //7,5,4,
+        4,6,7,
+        //0,4,6,
+        //2,0,6,
+        //0,1,4,
+
+    ];
+
+    // Initialize all of our WebGL "plumbing" variables
+    //
+    let aPosition = new Attribute(gl, program, positions,
+        "aPosition", 3, gl.FLOAT);
+
+    indices = new Indices(gl, indices);
+
+    let MV = new Uniform(gl, program, "MV");
+    let P  = new Uniform(gl, program, "P");
+
+    this.render = () => {
+        gl.useProgram(program);
+
+        aPosition.enable();
+        indices.enable();
+
+        MV.update(this.MV);
+        P.update(this.P);
+
+        gl.drawElements(gl.TRIANGLES, indices.count, indices.type, 0);
+
+        indices.disable();
+        aPosition.disable();
+
+    };
+};
---
+++ main.js
@@ -0,0 +1,31 @@
+var canvas = undefined;
+var gl = undefined;
+var time = 0.0;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    gl = canvas.getContext("webgl2");
+
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+
+    square = new Square(gl);
+
+    square.P = mat4();
+    square.MV = mat4();
+
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    time += 1.0;
+
+    square.MV = rotateY(time);
+
+    square.render();
+
+    requestAnimationFrame(render);
+}
+
+window.onload = init;
